b7d159daa89eeea6a804fdf3c5596ec07593e283
m1pty <109878771+m1pty@users.noreply.github.com>
2022-09-12 09:49:07 +0300

    Update 00-02_numsum.c

--- lab_00 (08.09.22)/00-02_numsum.c
+++ lab_00 (08.09.22)/00-02_numsum.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int counter(int x){
-    int answer = 0;
+    unsigned long answer = 0;
     while (x != 0){
         answer += x % 10;
         x /= 10;
@@ -12,6 +13,7 @@ int counter(int x){
 
 int main(){
     int number = 0;
+    setlocale(LC_ALL, "Russian");
     printf("Введите число для подсчёта суммы цифр: ");
     scanf("%d", &number);
     number = counter(number);
